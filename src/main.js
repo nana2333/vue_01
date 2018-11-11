0826d89a04d372d5bd7708650ab8b1c21ab9cf64
zhao <987878854@qq.com>
2018-11-11 12:33:05 +0800

    '静态页面'

---
+++ src/main.js
@@ -0,0 +1,15 @@
+/**
+ * Created by acer on 2018/11/11.
+ */
+/*入口文件*/
+import Vue from 'vue';
+import App from './App.vue';
+import './index.css'
+
+new Vue({
+  el:'#app',
+  components:{
+    App
+  },
+  template:'<App/>'
+})
